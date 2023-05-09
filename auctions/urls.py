8cc3476264bacb584bc9f9469851d0efdc31eb59
Mark Ortega <mortegaponce27@gmail.com>
2023-05-09 16:22:47 -0500

    auctions/urls: Updated the import name to be more explicit.

--- auctions/urls.py
+++ auctions/urls.py
@@ -1,17 +1,18 @@
 from django.urls import path
 
-from . import views
+from . import views as view_routes
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
-    path("create_listing", views.create_listing, name="create_listing"),
+    path("", view_routes.index, name="index"),
+    path("login", view_routes.login_view, name="login"),
+    path("logout", view_routes.logout_view, name="logout"),
+    path("register", view_routes.register, name="register"),
+    path("create_listing", view_routes.create_listing, name="create_listing"),
     # if were going to look at a certain listing, we need to at least know the listing id
     # can't remember syntax, so fine to look at notes
-    path("listing_page/<int:auction_id>", views.listing_page, name="listing_page"),
-    path("listing_page/<int:auction_id>/watchlist", views.watchlist, name="watchlist"),
-    path('listing_page/<int:auction_id>/new_bid', views.new_bid, name="new_bid")
-    # path("listing_page/<int:auction_id>/comment", views.listing_page_comment, name="listing_page_comment")
+    path("listing_page/<int:auction_id>", view_routes.listing_page, name="listing_page"),
+    path("listing_page/watchlist", view_routes.watchlist, name="watchlist"),
+    path("listing_page/<int:auction_id>/add_watchlist", view_routes.add_watchlist, name="add_watchlist"),
+    path('listing_page/<int:auction_id>/new_bid', view_routes.new_bid, name="new_bid")
+    # path("listing_page/<int:auction_id>/comment", view_routes.listing_page_comment, name="listing_page_comment")
 ]
