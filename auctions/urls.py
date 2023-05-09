57c3c7e6f89a2ff585d6b2c58afb13d040772a78
Mark Ortega <mortegaponce27@gmail.com>
2023-05-09 16:41:28 -0500

    auctions/urls.py: Added categories url path.

--- auctions/urls.py
+++ auctions/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path("listing_page/<int:auction_id>", view_routes.listing_page, name="listing_page"),
     path("listing_page/watchlist", view_routes.watchlist, name="watchlist"),
     path("listing_page/<int:auction_id>/add_watchlist", view_routes.add_watchlist, name="add_watchlist"),
-    path('listing_page/<int:auction_id>/new_bid', view_routes.new_bid, name="new_bid")
+    path('listing_page/<int:auction_id>/new_bid', view_routes.new_bid, name="new_bid"),
+    path("listing_page/categories", view_routes.categories, name="categories")
     # path("listing_page/<int:auction_id>/comment", view_routes.listing_page_comment, name="listing_page_comment")
 ]
