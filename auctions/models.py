32235ca9872e79bf7a00e062197c1af60ae78434
Mark Ortega <mortegaponce27@gmail.com>
2023-05-10 17:26:18 -0500

    auctions/models.py: Cleaning up some of the comments.

--- auctions/models.py
+++ auctions/models.py
@@ -1,24 +1,15 @@
-# https://docs.djangoproject.com/en/4.1/topics/auth/customizing/
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django import forms
-# possible fiels for models
-# here is more general information about fields and more
+
+# https://docs.djangoproject.com/en/4.1/topics/auth/customizing/
 # https://docs.djangoproject.com/en/4.0/ref/models/fields/
-# Here is an anchor tag link to all the fiels available to use (REALLY USEFUL)
 # https://docs.djangoproject.com/en/4.0/ref/models/fields/#field-types
-
-'''
-Thing to remember, Django gives each model an
-auto-incrementing primary key, so do not have to define ourselves.
-
-https://docs.djangoproject.com/en/4.0/topics/db/models/#automatic-primary-key-fields
-
-So to refer to it, we would use it as a ForeignKey if that makes sense
-'''
-
+# https://docs.djangoproject.com/en/4.0/topics/db/models/#automatic-primary-key-fields
 # https://docs.djangoproject.com/en/4.0/ref/models/fields/#choices
+# https://docs.djangoproject.com/en/4.1/ref/contrib/auth/
+
 class Categories(models.TextChoices):
     CLOTHING = "Clothing", _('Clothing')
     SHOES = "Shoes", _('Shoes')
@@ -43,33 +34,20 @@ class User(AbstractUser):
     # It is recommended to use Abstract User even if User class
     # is enough, because it is more customizable.
     # Can add additional fields here. (We inherit all fiels from default User class)
-    # https://docs.djangoproject.com/en/4.1/ref/contrib/auth/
-
-
     def __str__(self):
-        # id is autoincremented, and it is suggested not to add the field yourself
+        # Id is autoincremented. It is suggested not to add the field yourself.
         return f"User: {self.username} Id: {self.id}"
     
 class AuctionListing(models.Model):
 
-    # pull some ideas 'create listing' specification
-    # to create a listing, user should be able to specify
-    # title
-    # description
-    # starting bid price
-    # url to picture/image (not required for user)
-    # category (not required for user)
-
-    # When user is deleted
-    # delete all Auction Listings associated with user
+    # Preset choices of categories, eliminate checking user input.
     category = models.CharField(
         max_length=16,
         choices=Categories.choices,
         default=Categories.OTHER,
     )
     # https://stackoverflow.com/questions/41595364/fields-e304-reverse-accessor-clashes-in-django
-    user = models.ForeignKey(User, on_delete=models.CASCADE,
-                             related_name="creator")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     # Would run into issues if we allowed user to remove bid, but good for now.
     highest_bidder_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidder",null=True, blank=True)
     auction_title = models.CharField(max_length=64)
@@ -88,7 +66,6 @@ class AuctionListing(models.Model):
             url = "N/A\n"
         else:
             url = f"Url: {self.image_url}\n"
-
         if len(self.category) == 0:
             category = "N/A\n"
         else:
@@ -97,13 +74,12 @@ class AuctionListing(models.Model):
         return user + title + start_bid + desc + url + category
 
 class Comment(models.Model):
-    # comment is placed by logged-in user
-    # have user_id associated with comment
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # have comment field
     comment = models.TextField()
     time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     # https://cs50.harvard.edu/web/2020/notes/4/ (for some examples of how to implement models)
+    # By using the related name, this allows us to look for all comments associated with a specific auction!
     auction = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="comments", blank=True, null=True)
     
     def __str__(self):
@@ -112,12 +88,11 @@ class Comment(models.Model):
         return user + comment
 
 class Bid(models.Model):
-    # bid is places by logged-in user
-    # need user_id to associate with a user
+    # Bid is places by logged-in user.
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # have item key to associate bid
+    # Associate bid with auction listing.
     auction_id = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
-    # need a bid_price as float
+    # Need a bid_price as float.
     # DecimalField has two required arguments
     bid_price = models.DecimalField(max_digits=8, decimal_places=2)
 
@@ -128,10 +103,9 @@ class Bid(models.Model):
         return user + auction_id + bid_price
 
 class Watchlist(models.Model):
-    # if user is ever deleted, delete any associated watchlists rows
-    # given a User, gives us a way to look at any watchlist item associated with our user (related name)
-    # This lets us retrieve all the users with a specific item as their watchlist
-    # we want the opposite effect.
+    # If user is ever deleted, delete any associated watchlists rows.
+    # Given a User. Give us a way to look at any watchlist item associated with our user (related name). 
+    # e.g. user_obj.watchlist_items
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="watchlist_items")
     # if Auction Listing item is removed, delete from watchlist
     item = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
