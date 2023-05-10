a59e712fbf5e45d44c79fbbd5e7028d57ec682c1
Mark Ortega <mortegaponce27@gmail.com>
2023-05-10 14:59:10 -0500

    auctions/urls.py: Added route to add comments.

--- auctions/urls.py
+++ auctions/urls.py
@@ -11,9 +11,13 @@ urlpatterns = [
     # if were going to look at a certain listing, we need to at least know the listing id
     # can't remember syntax, so fine to look at notes
     path("listing_page/<int:auction_id>", view_routes.listing_page, name="listing_page"),
-    path("listing_page/watchlist", view_routes.watchlist, name="watchlist"),
     path("listing_page/<int:auction_id>/add_watchlist", view_routes.add_watchlist, name="add_watchlist"),
     path('listing_page/<int:auction_id>/new_bid', view_routes.new_bid, name="new_bid"),
-    path("listing_page/categories", view_routes.categories, name="categories")
+    # Consider removeing listing_page/ from other url paths if not needed, I did for bottom two.
+    path("watchlist", view_routes.watchlist, name="watchlist"),
+    path("categories", view_routes.categories, name="categories"),
+    path("categories/<str:category>", view_routes.category, name="category"),
+    path("cancel/<int:auction_id>", view_routes.cancel, name="cancel"),
+    path("comment/<int:auction_id>", view_routes.comment, name="comment")
     # path("listing_page/<int:auction_id>/comment", view_routes.listing_page_comment, name="listing_page_comment")
 ]
